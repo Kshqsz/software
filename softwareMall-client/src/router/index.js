fa079961e382b48cafe46220509c9549b6cd7b21
hanshoukun <2456480538@qq.com>
2024-11-06 15:41:43 +0800

    详细设计说明书上传

--- softwareMall-client/src/router/index.js
+++ softwareMall-client/src/router/index.js
@@ -2,7 +2,18 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: 
+  [
+    {
+      path: '/login',
+      component: () => import ('@/views/login/LoginPage.vue')
+    },
+
+    {
+      path: '/',
+      component: () => import ('@/views/login/LoginPage.vue')
+    }
+  ],
 })
 
 export default router
